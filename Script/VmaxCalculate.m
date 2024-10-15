d58bf9987ef477367b8c95d69065d3532abaed6d
Knight—lin <755437485@qq.com>
2024-10-15 10:34:48 +0800

    update

---
+++ Script/VmaxCalculate.m
@@ -0,0 +1,32 @@
+Table_UniKP=readtable('Database/UniKP_Result.xlsx');
+load('Data_trans.mat', 'EXPO')
+Kcat=Table_UniKP.Kcat;
+Km=Table_UniKP.Km;
+Name=Table_UniKP.ID;
+
+S = logspace(-3, 3, 7);
+
+
+
+TPM=EXPO.CS6;
+GeneID=EXPO.Gene;
+[C,IA,IC]=intersect(GeneID,Name);
+
+E=mean(TPM(IA,:),2);
+
+Km_1=Km(IC);
+Kcat_1=Kcat(IC);
+Ratio=[];
+
+
+
+for x=1:length(Kcat_1)    
+    for y=1:7
+Test=(E(x)*Kcat_1(x))/(Km_1(x)+S(y));    
+Total = sum((E.*Kcat_1 )./ (Km_1 + S(y)));
+Ratio(x,y)=Test/Total;
+    end
+end
+
+Name(IC)
+
